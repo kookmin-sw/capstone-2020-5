6eade9315838f44ae2615e2421fa43dab2631336
Ruslan Yun <leonemsolis@gmail.com>
2020-04-14 16:09:35 +0900

    Add multiple file upload

--- front/flask-app/main.py
+++ front/flask-app/main.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, render_template, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__, static_folder='../react-app/build')
 
@@ -11,6 +12,14 @@ def serve(path):
         return send_from_directory(app.static_folder, path)
     else:
         return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/upload-files', methods = ['POST'])
+def upload_file():
+    uploaded_files = request.files.getlist("fileCollection")
+    for file in uploaded_files:
+        file.save(secure_filename(file.filename))
+
+    return 'Successfuly recieved and saved files'
 
 
 if __name__ == '__main__':
