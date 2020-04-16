d92357c97c1547240aecbf44c0df707296c3c042
yerinfive <yerinoh@kookmin.ac.kr>
2020-04-16 20:31:55 +0900

    ngram 필드 구조 변경

--- elasticsearch/bulkFiveGram.py
+++ elasticsearch/bulkFiveGram.py
@@ -3,9 +3,6 @@ import json
 import glob
 import os
 
-def divide_list(l):
-    for i in range(0, len(l), 5):
-        yield l[i:i + 5]
 
 files = glob.glob(r"/media/hyeongy/Seagate Backup Plus Drive/hyeongy_capstone/ida/mal/*.pickle")
 
@@ -26,8 +23,9 @@ with open("asem.json", "wb") as jsonFile:
                         continue
                     for k in j:
                         ngram.append(k)
-            file = list(divide_list(ngram))
-            file = list(set([tuple(item)) for item in file]))
+            for i in range (4, len(tmp)):
+                ngram.append(tmp[i-4]+" "+tmp[i-3]+" "+tmp[i-2]+" "+tmp[i-1]+" "+tmp[i])
+            file = list(set(ngram))
 
             print(file)
 
