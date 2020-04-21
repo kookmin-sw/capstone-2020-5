fa88877029de3a3cbfde335cbf0969c92865247c
Ruslan Yun <leonemsolis@gmail.com>
2020-04-21 22:48:34 +0900

    Moved styles to css files

--- front/react-app/src/Inspect/Mnemonic/InnerDialog/InnerDialog.js
+++ front/react-app/src/Inspect/Mnemonic/InnerDialog/InnerDialog.js
@@ -1,41 +1,13 @@
 import React, { Component } from 'react';
 import "./InnerDialog.css"
 
-let dialogStyles={
-    width:'500px',
-    maxWidth:'100%',
-    margin:'0 auto',
-    positiion:'fixed',
-    left:'50%',
-    top:'50%',
-    transform:'translate(-50%,50%)',
-    zIndex:'999',
-    backgroundColor:'#eee',
-    padding:'10px 20px 40px',
-    borderRadius:'8px',
-    display:'flex',
-    flexDirection:'column'
-};
 
-let diaglogCloseButtonStyles={
-    marginBottom:'15px',
-    padding:'3px 8px',
-    cursor:'pointer',
-    borderRadius:'50%',
-    border:'none',
-    width:'30px',
-    height:'30px',
-    fontWeight:'bold',
-    alignSelf:'flex-end'
-};
 
 
 class InnerDialog extends Component {
     constructor(props) {
         super(props);
     }
-
-
 
     createListOfMnemonics() {
         let listOfFunc = []
@@ -50,8 +22,8 @@ class InnerDialog extends Component {
 
     render() {
         return (
-        <div style={dialogStyles} >
-            <button style={diaglogCloseButtonStyles} onClick={this.props.onClose}>X</button>
+        <div id="dialogStyles" >
+            <button id="diaglogCloseButtonStyles" onClick={this.props.onClose}>X</button>
             <div id="scroll-view-innerDialog">
             {this.createListOfMnemonics()}
             </div>
--- front/react-app/src/Inspect/Mnemonic/Mnemonic.js
+++ front/react-app/src/Inspect/Mnemonic/Mnemonic.js
@@ -2,35 +2,6 @@ import React, { Component } from 'react';
 import "./Mnemonic.css"
 import InnerDialog from "./InnerDialog/InnerDialog";
 
-let dialogStyles={
-    width:'500px',
-    maxWidth:'100%',
-    margin:'0 auto',
-    positiion:'fixed',
-    left:'50%',
-    top:'50%',
-    transform:'translate(-50%,50%)',
-    zIndex:'999',
-    backgroundColor:'#eee',
-    padding:'10px 20px 40px',
-    borderRadius:'8px',
-    display:'flex',
-    flexDirection:'column'
-};
-
-let diaglogCloseButtonStyles={
-    marginBottom:'15px',
-    padding:'3px 8px',
-    cursor:'pointer',
-    borderRadius:'50%',
-    border:'none',
-    width:'30px',
-    height:'30px',
-    fontWeight:'bold',
-    alignSelf:'flex-end'
-};
-
-
 class Mnemonic extends Component {
     constructor(props) {
         super(props);
@@ -40,10 +11,6 @@ class Mnemonic extends Component {
         Object.entries(this.mal_functions).forEach(([key, value]) => {
             this.indices.push(key);
         });
-
-        // console.log(indices);
-        // console.log(mal_functions);
-        // console.log(mal_functions[indices[2]])
     }
 
 
@@ -58,14 +25,14 @@ class Mnemonic extends Component {
                 }}>{this.indices[i]}</button>
             );
         }
-        return listOfFunc
+        return listOfFunc;
     }
  
 
     render() {
         return (
-        <div style={dialogStyles} >
-            <button style={diaglogCloseButtonStyles} onClick={this.props.onClose}>X</button>
+        <div id="dialogStyles">
+            <button id="diaglogCloseButtonStyles" onClick={this.props.onClose}>X</button>
             <div id="scroll-view-mnemonic">
                 {this.createListOfFunc()}
             </div>
