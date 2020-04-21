f7d2dc7d28d9e487062ee51da1c398617900076d
ghoxa <hotem1234@gmail.com>
2020-04-21 17:22:36 +0900

    add main.py comment

--- front/react-app/src/Inspect/Mnemonic/Mnemonic.js
+++ front/react-app/src/Inspect/Mnemonic/Mnemonic.js
@@ -1,24 +1,39 @@
 import React, { Component } from 'react';
-
+import "./Mnemonic.css"
 class Mnemonic extends Component {
-    constructor(props){
+    constructor(props) {
+        state={
+            isOpen:false
+        }
         super(props);
 
-        var mal_functions=JSON.parse(window.localStorage.getItem('data'))[this.props.filename]["mal_functions"];
-        var indices = [];
-
-        Object.entries(mal_functions).forEach(([key, value]) => {
-            indices.push(key);
+        this.mal_functions = JSON.parse(window.localStorage.getItem('data'))[this.props.filename]["mal_functions"];
+        this.indices = [];
+        Object.entries(this.mal_functions).forEach(([key, value]) => {
+            this.indices.push(key);
         });
-        console.log(indices);
-        console.log(mal_functions);
-        console.log(mal_functions[indices[2]])
+
+        // console.log(indices);
+        // console.log(mal_functions);
+        // console.log(mal_functions[indices[2]])
     }
-    
+    createListOfFunc() {
+        let listOfFunc = []
+        for (let i = 0; i < this.indices.length; i++) {
+            listOfFunc.push(
+                <p key={i}>{this.indices[i]}</p>
+            )
+
+
+        }
+        return listOfFunc
+    }
+
+
     render() {
         return (
-            <div>
-                
+            <div className="scroll-view" >
+                {this.createListOfFunc()}
             </div>
         );
     }
