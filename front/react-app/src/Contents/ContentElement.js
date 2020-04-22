41ced690d4468d8a130ead7df06224aec18082af
ghoxa <hotem1234@gmail.com>
2020-04-22 13:26:32 +0900

    fix col size

--- front/react-app/src/Contents/ContentElement.js
+++ front/react-app/src/Contents/ContentElement.js
@@ -9,7 +9,7 @@ class ContentElement extends Component{
                 <h3 className="col-3">{this.props.filename}</h3>
                 <h3 className="col-7"></h3>
                 <Link to={"/contents/" + this.props.filename}>
-                    <h3 type="button" className="col-1 content-element-button btn btn-success">결과 보기</h3>
+                    <h3 type="button" className="col-15 content-element-button btn btn-success">결과 보기</h3>
                 </Link>
             </div>
         );
