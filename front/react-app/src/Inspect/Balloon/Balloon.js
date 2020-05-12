cdead4967b23b33381231276c4094b52356418ec
ghoxa <hotem1234@gmail.com>
2020-05-12 18:00:46 +0900

    add metadata

--- front/react-app/src/Inspect/Balloon/Balloon.js
+++ front/react-app/src/Inspect/Balloon/Balloon.js
@@ -5,10 +5,15 @@ class Balloon extends Component{
     constructor(props) {
         super(props);
         var ballonData = (this.props.file["samefile"]);
+        var meta=(this.props.file["meta"]);
+        this.md5=meta["md5"];
+        this.sha256=meta["sha256"];
+        this.size=meta["size"];
         this.malhits = ballonData["mal"]["hits"];
         this.malscore = ballonData["mal"]["score"];
         this.benhits = ballonData["ben"]["hits"];
         this.benscore = ballonData["ben"]["score"]; 
+       
     }
 
     render() {
@@ -28,7 +33,20 @@ class Balloon extends Component{
                     <p>Score: {this.benscore}</p>
                     <p>Hits: {this.malhits}</p>
                 </div> 
+            
+
+             
+             <div className="balloon mal">
+                 <br/>
+                 <h1>Meta</h1>
+                 <br/>
+                 <p className="mal_score">Md5: {this.md5}</p>
+
+                 <p>Sha256: {this.sha256}</p>
+                 <p>Size:{this.size}</p>
+             </div>    
             </div>
+         
         );
     }
 }
