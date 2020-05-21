47f5dfa3dfd91523518b9c3c8f1ac7b60d6910fd
Ruslan Yun <leonemsolis@gmail.com>
2020-05-22 00:51:13 +0900

    Revert "Inspector add meta + samefile data"
    This reverts commit d95f68c7011b02ace4944c2477786a52a5b8121b.

--- front/react-app/src/Inspect/Inspect.js
+++ front/react-app/src/Inspect/Inspect.js
@@ -18,12 +18,8 @@ class Inspect extends Component {
             mnemonicIndices : null,
             stringIndices : null,
             importIndices : null,
-            exportIndices : null,
-            meta: null,
-            samefile: null
+            exportIndices : null
         }
-        this.showMeta = this.showMeta.bind(this);
-        this.showSameFile = this.showSameFile.bind(this);
         this.createListOfInnerElements = this.createListOfInnerElements.bind(this);
         this.createListOfIndices = this.createListOfIndices.bind(this);
         this.downloadFile = this.downloadFile.bind(this);
@@ -58,10 +54,7 @@ class Inspect extends Component {
                 window.location = "/error";
             } else {
                 this.setState({ file: response.data });
-
-                this.setState({meta: this.state.file["meta"]});
-                this.setState({samefile: this.state.file["samefile"]});
-
+                
                 this.mnemonics = this.state.file["mal_functions"];
                 var mnemonicIndicesTemp = [];
                 Object.entries(this.mnemonics).forEach(([key, value]) => {
@@ -107,7 +100,7 @@ class Inspect extends Component {
         let listOfFunc = [];
         for (let i = 0; i < indices.length; i++) {
             listOfFunc.push(
-                <div key={i}><h2>{indices[i]}</h2>
+                <div key={i}>{indices[i]}
                     <br/>
                     {
                         this.createListOfInnerElements(indices[i], innerElements)
@@ -118,216 +111,9 @@ class Inspect extends Component {
         return listOfFunc;
     }
 
-    showSameFile() {
-        return(
-            <div>
-                <p>MAL HITS {this.state.samefile["mal"]["hits"]}</p>
-                <p>MAL SCORE {this.state.samefile["mal"]["score"]}</p>
-                <p>BEN HITS {this.state.samefile["ben"]["hits"]}</p>
-                <p>BEN SCORE {this.state.samefile["ben"]["score"]}</p>
-            </div>
-        );
-    }
-
-    showMeta() {
-        return(
-            <div>
-                <p>MD5 {this.state.meta["md5"]}</p>
-                <p>SHA256 {this.state.meta["sha256"]}</p>
-            </div>
-        );
-    }
-
     render() {
         return(
-<<<<<<< HEAD
-            <div className="sample_container">
-                <Nav />
-                <div className="sample_body">
-                    <div className="file_info">
-                        <div className="donut_chart">
-                            <div className="white_donut">
-                                <div className="chart_contents"> 80% </div>
-                            </div>
-                        </div>
-                        <div className="file_contents">
-                            <div className="file_name"> file name</div>
-                            <div className="file_meta">
-                                <li className="file_md5"> md5 : 	55cb06fc7ddebaf8c87df15c3681a1fd</li>
-                                <li className="file_sha256"> sha256 : 	b384422960a820e3091e011d1a74d6cb5f5fb9f98a67e88233c7da1e3f91e778 </li>
-                                <li className="file_size"> file size : 440.92 KB</li>
-                            </div>
-                        </div>
-                    </div>
-                    <hr className="under_line"></hr>
-                    <div className="mnemonic">
-                        <div className="contents_title">Mnimonic</div>
-                        <ul className="nav nav-tabs data_tabs">
-                            <li className="nav-item">
-                                <a className="nav-link active" data-toggle="tab" href="#30">30</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" data-toggle="tab" href="#30">30</a>
-                            </li>
-                        </ul>
-                        <div className="tab-content">
-                            <div className="tab-pane tab_contents fade show active" id="qwe">
-                                <p>
-                                    {this.state.mnemonicIndices == null ? <br/> : this.createListOfIndices(this.state.mnemonicIndices, this.mnemonics)}
-                                </p>
-                            </div>
-                            <div className="tab-pane tab_contents fade" id="asd">
-                                <p>
-                                    {this.state.stringIndices == null ? <br/> : this.createListOfIndices(this.state.stringIndices, this.string)}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="string">
-                        <div className="contents_title">String</div>
-                        <ul className="nav nav-tabs data_tabs">
-                            <li className="nav-item">
-                                <a className="nav-link active" data-toggle="tab" href="#m1">30</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" data-toggle="tab" href="#m2">30</a>
-                            </li>
-                        </ul>
-                        <div className="tab-content">
-                            <div className="tab-pane tab_contents fade show active" id="m1">
-                                <p>
-                                    {this.state.stringIndices == null ? <br/> : this.createListOfIndices(this.state.stringIndices, this.string)}
-
-                                </p>
-                            </div>
-                            <div className="tab-pane tab_contents fade" id="m2">
-                                <p>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="import">
-                        <div className="contents_title">Import</div>
-                        <ul className="nav nav-tabs data_tabs">
-                            <li className="nav-item">
-                                <a className="nav-link active" data-toggle="tab" href="#30">30</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" data-toggle="tab" href="#30">30</a>
-                            </li>
-                        </ul>
-                        <div className="tab-content">
-                            <div className="tab-pane tab_contents fade show active" id="qwe">
-                                <p>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                </p>
-                            </div>
-                            <div className="tab-pane tab_contents fade" id="asd">
-                                <p>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="Export">
-                        <div className="contents_title">Export</div>
-                        <ul className="nav nav-tabs data_tabs">
-                            <li className="nav-item">
-                                <a className="nav-link active" data-toggle="tab" href="#30">30</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" data-toggle="tab" href="#30">30</a>
-                            </li>
-                        </ul>
-                        <div className="tab-content">
-                            <div className="tab-pane tab_contents fade show active" id="qwe">
-                                <p>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                </p>
-                            </div>
-                            <div className="tab-pane tab_contents fade" id="asd">
-                                <p>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                    add<br></br>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                {/*<div>
-            MNEMONIC: {this.state.mnemonicIndices == null ? <br/> : this.createListOfIndices(this.state.mnemonicIndices, this.mnemonics)}
-=======
-            <div>
-            <h1>META:</h1> {this.state.meta == null ? <br/> : this.showMeta()}
-            <br/>
-            <h1>SAMEFILE:</h1> {this.state.samefile == null ? <br/> : this.showSameFile()}
-            <br/>
-            <h1>MNEMONIC:</h1> {this.state.mnemonicIndices == null ? <br/> : this.createListOfIndices(this.state.mnemonicIndices, this.mnemonics)}
->>>>>>> d95f68c7011b02ace4944c2477786a52a5b8121b
-            <br/>
-            <h1>STRING:</h1> {this.state.stringIndices == null ? <br/> : this.createListOfIndices(this.state.stringIndices, this.string)}
-            <br/>
-            <h1>IMPORT:</h1> {this.state.importIndices == null ? <br/> : this.createListOfIndices(this.state.importIndices, this.import)}
-            <br/>
-<<<<<<< HEAD
-            EXPORT: {this.state.exportIndices == null ? <br/> : this.createListOfIndices(this.state.exportIndices, this.export)}
-            </div>*/}
-=======
-            <h1>EXPORT:</h1> {this.state.exportIndices == null ? <br/> : this.createListOfIndices(this.state.exportIndices, this.export)}
->>>>>>> d95f68c7011b02ace4944c2477786a52a5b8121b
-            </div>
-
+            <div></div>
 
         );
     }
