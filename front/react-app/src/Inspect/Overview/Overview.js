cc8aa3404928900e6d5d9b799e86034fef118580
Ruslan Yun <leonemsolis@gmail.com>
2020-05-23 16:45:31 +0900

    Add Overview

--- front/react-app/src/Inspect/Overview/Overview.js
+++ front/react-app/src/Inspect/Overview/Overview.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
 import "./Overview.css"
+import "../Inspect.css";
+import "../sample.css";
 class Overview extends Component {
+
     render() {
         return (
-            <div className="container upload-page">
-
-                <div className="container vertical-element w-100">
-                    <h1>전체보기</h1>
+            <div className="card full_accordion">
+                <div className="accordion-header" id={"heading"+1}>
+                    <div className="mb-0 each_function">
+                        <div className="function_hash col-10">
+                            55cb06fc7ddebaf8c87df15c3681a1fd
+                        </div>
+                        <div className="result-button-contain col-2">
+                            <div className="result-button" data-toggle="collapse"
+                                    data-target={"#" + "md5"} aria-expanded="true"
+                                    aria-controls={"md5"}>
+                                유사도 검사
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div ref={this.dropInput} id="drop-area" className="container bg-light vertical-element vertical-center text-center">
-                    <form className="my-form w-100">
-
-                    </form>
-                </div>
-                <div className="container d-flex vertical-element">
-                    <div className="row w-100 justify-content-end">
-                        <input ref={this.fileInput} type="file" id="file" className="inputfile" multiple onChange={(e) => { this.files = e.target.files; this.forceUpdate(); }} />
-                        <label type="button" htmlFor="file" className="col-2 text-light text-center button btn btn-primary">목록으로</label>
-                        <p className="col-1"></p>
-                        <button id="button-upload" type="button" className="col-2 text-light text-center button btn btn-success" onClick={this.handleSubmit}>제출</button>
+                <div id={"md5"} className="collapse" aria-labelledby={"heading"+1}
+                     data-parent="#accordion">
+                    <div className="card-body">
+                        <div>
+                            progress bar
+                        </div>
+                        <div>
+                            유사한 md5   cosine   edit   보기
+                        </div>
                     </div>
                 </div>
             </div>
